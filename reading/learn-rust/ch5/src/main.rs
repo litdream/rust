2e16b16d37f937536488edc56c2448914a62ce05
Ray Chung <rchung725@gmail.com>
2026-02-11 05:13:34 -0500

    ch5 done:  struct and impl(method)

--- reading/learn-rust/ch5/src/main.rs
+++ reading/learn-rust/ch5/src/main.rs
@@ -1,13 +1,44 @@
 fn main() {
+    basic();
+    method1();
+    
+}
+
+fn basic() {
     let width1 = 30;
     let height1 = 50;
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        func_area(width1, height1)
     );
+
+}
+fn func_area(width: u32, height: u32) -> u32 {
+    width * height
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+//
+
+#[derive(Debug)]
+struct Retangle {
+    width: u32,
+    height: u32,
+}
+impl Retangle {
+    fn area(&self) -> u32 {
+	self.width * self.height
+    }
+}
+
+fn method1() {
+    let rect1 = Retangle {
+	width: 30,
+	height: 50,
+    };
+
+    println!(
+	"The area of the rectangle is {} square pixels.",
+	rect1.area()
+    );
 }
